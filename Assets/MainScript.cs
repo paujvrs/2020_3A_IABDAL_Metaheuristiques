c5a8891b292dc537581b30ebfc484add6421c18a
Nicolas Vidal <pro@nicolasvidal.fr>
2019-10-29 19:41:06 +0100

    Cubes world Setup Complete git add -A! :)

---
+++ Assets/MainScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] cubes;
+    
+    // Start is called before the first frame update
+    private void Start() 
+    {
+        Debug.Log($"Hello les 3A IABDAL !!! :)");
+        Debug.Log($"Il y a {cubes.Length} cubes dans la scene !!!");
+    }
+}
